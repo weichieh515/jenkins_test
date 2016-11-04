ecf30851dcdb0f06909a61aacb3fe1ac0e0274d7
GIGABYTE-Z170N\Weichieh <ace05152535@gmail.com>
2016-11-04 20:50:28 +0800

    jenkins test commit

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   response.end('Hello World\n it is updated by jenkins');
 }).listen(8081);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8082/');
